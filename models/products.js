c4f25b55e162a8f4f5b67c1d47a370c59a847027
DESKTOP-CD0NI2Q\juanm <juanmluces@gmail.com>
2021-01-23 12:20:08 +0100

    metodos y rutas busqueda por nombre

--- models/products.js
+++ models/products.js
@@ -1,8 +1,8 @@
 const getAllCategories = () => {
   return new Promise((resolve, reject) => {
-    db.query('SELECT * from categories', (err, rows) =>{
-      if(err) reject(err);
-      if(rows.length === 0) resolve(null);
+    db.query('SELECT * from categories', (err, rows) => {
+      if (err) reject(err);
+      if (rows.length === 0) resolve(null);
       resolve(rows);
     })
   })
@@ -11,11 +11,29 @@ const getAllCategories = () => {
 const getCategoryById = (pId) => {
   return new Promise((resolve, reject) => {
     db.query('SELECT * from categories WHERE id = ?', [pId], (err, rows) => {
-      if(err) reject(err);
-      if(rows.length === 0) resolve(null);
+      if (err) reject(err);
+      if (rows.length === 0) resolve(null);
       resolve(rows[0]);
     })
   })
 }
 
-module.exports = { getAllCategories, getCategoryById }
+const getAllProducts = () => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * from products', (err, rows) => {
+      if (err) reject(err);
+      resolve(rows)
+    })
+  })
+}
+
+const getProductsContainsName = (pName) => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM products WHERE nombre LIKE ? OR marca LIKE ?', [`%${pName}%`, `%${pName}%`], (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    })
+  })
+}
+
+module.exports = { getAllCategories, getCategoryById, getAllProducts, getProductsContainsName }
--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,39 @@
 const router = require('express').Router();
-const {getAllCategories, getCategoryById} = require('../models/products')
+const { getAllCategories, getCategoryById, getAllProducts, getProductsContainsName } = require('../models/products')
 
 
-router.get('/categories', async(req, res) => {
-  try{
+router.get('/', async (req, res) => {
+  try {
+    const products = await getAllProducts();
+    res.json(products)
+  } catch (err) {
+    res.json({ err: err.message })
+  }
+});
+
+router.get('/search/:name', async (req, res) => {
+  const searchName = req.params.name;
+  try {
+    const products = await getProductsContainsName(searchName);
+    res.json(products)
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+router.get('/categories', async (req, res) => {
+  try {
     const categories = await getAllCategories();
     res.json(categories)
-  }catch(error){ res.json({error: error.message})}
+  } catch (error) { res.json({ error: error.message }) }
 })
 
 router.get('/categories/:id', async (req, res) => {
   const catId = req.params.id;
-  try{
+  try {
     const categoría = await getCategoryById(catId);
     res.json(categoría)
-  }catch(error){res.json({error: error.message})}
+  } catch (error) { res.json({ error: error.message }) }
 })
 
 module.exports = router;
