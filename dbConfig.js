58e9312f804c5d5d8a8d6bf47b6c768e6c28c8e6
DESKTOP-CD0NI2Q\juanm <juanmluces@gmail.com>
2021-01-30 14:08:44 +0100

    first commit

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ const mySql = require('mysql');
 require('dotenv').config();
 
 const createPool = () => {
-  const pool =  mySql.createPool({
+  const pool = mySql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
