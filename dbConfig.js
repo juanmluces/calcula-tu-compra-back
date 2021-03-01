41444f36d7a3ad6993158c5da8b6bcd3b7d54b43
DESKTOP-CD0NI2Q\juanm <juanmluces@gmail.com>
2021-03-01 11:26:15 +0100

    env virables

--- dbConfig.js
+++ dbConfig.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 const createPool = () => {
   const pool = mySql.createPool({
-    host: 'eu-cdbr-west-03.cleardb.net',
-    user: 'b1b686519824fe',
-    password: '38955cae',
-    port: 3306,
-    database: 'heroku_fe6a2ff421738a8'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
   });
   global.db = pool
 }
