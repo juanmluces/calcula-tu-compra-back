8a88a0d170bcf5e548a2c708e075f0f16237759e
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2021-09-26 14:02:43 +0200

    TEST FIX

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express();
 app.use(cors())
 
 // view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -27,7 +28,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,6 +37,7 @@ app.use('/profile', checkToken, profileRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  console.log('error 404');
   next(createError(404));
 });
 
@@ -48,6 +49,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  console.log('errror500');
   res.render('error');
 });
 
