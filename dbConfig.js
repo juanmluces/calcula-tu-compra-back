a2e3b08d0bde3ac5eb1c3fd70c720871c0efa2cf
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2024-09-03 08:21:35 +0200

    add db and docker compose

--- dbConfig.js
+++ dbConfig.js
@@ -7,7 +7,7 @@ const createPool = () => {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
-		// port: process.env.DB_PORT,
+		port: process.env.DB_PORT,
 		database: process.env.DB_NAME,
 	});
 	global.db = pool;
